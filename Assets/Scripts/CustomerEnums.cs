31ef9650bb4dfc75f95d538b504996c872bb1e14
Erza2727 <eyrun.inga.einarsdottir@gmail.com>
2025-12-03 10:39:54 +0000

    .

---
+++ Assets/Scripts/CustomerEnums.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CustomerEnums
+{
+
+    public enum Archetype
+{
+    Grumpy,
+    Tired,
+    Happy
+}
+
+public enum PizzaType
+{
+    Pepperoni,
+    Hawaiian,
+    Vegan
+}
+
+public enum EyeColor
+{
+    Red,
+    Blue,
+    Brown
+}
+
+    
+}
+
