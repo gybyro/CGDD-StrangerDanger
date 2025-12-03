a89365b564893bc244c07b29544a405952dfa414
goji <100840760+gybyro@users.noreply.github.com>
2025-12-03 17:03:15 +0000

    fades in ;/

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,6 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    public CanvasGroup fadeGroup;
-    public float fadeTime = 1f;
 
     [System.Serializable]
     public class CustomerData
@@ -144,38 +142,4 @@ public class GameManager : MonoBehaviour
         return $"{currentActual.name}, Age {currentActual.age}, Works at {currentActual.workplace}";
     }
 
-        // ========================== Scene Fade =====================================
-    public void FadeToScene(string sceneName)
-    {
-        StartCoroutine(FadeIn(sceneName));
-    }
-
-    IEnumerator FadeIn(string sceneName)
-    {
-        float t = 0;
-
-        while (t < fadeTime)
-        {
-            t += Time.deltaTime;
-            fadeGroup.alpha = t / fadeTime;
-            yield return null;
-        }
-
-        SceneManager.LoadScene(sceneName);
-
-        // Now fade out after the scene loads
-        yield return StartCoroutine(FadeOut());
-    }
-
-    IEnumerator FadeOut()
-    {
-        float t = 0;
-
-        while (t < fadeTime)
-        {
-            t += Time.deltaTime;
-            fadeGroup.alpha = 1 - (t / fadeTime);
-            yield return null;
-        }
-    }
 }
--- Assets/Scripts/SceneTransitionScript.cs
+++ Assets/Scripts/SceneTransitionScript.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneTransition : MonoBehaviour
 {
     public string sceneNameToLoad;
+    public CanvasGroup fadeGroup;
+    public float fadeTime = 1f;
     
     public void LoadScene()
     {
@@ -16,8 +19,8 @@ public class SceneTransition : MonoBehaviour
         // Check if the thing entering is the player
         if (other.CompareTag("Player"))
         {
-            //GameManager.Instance.FadeToScene(sceneNameToLoad);
-            SceneManager.LoadScene(sceneNameToLoad);
+            FadeToScene(sceneNameToLoad);
+            // SceneManager.LoadScene(sceneNameToLoad);
         }
     }
 
@@ -25,5 +28,43 @@ public class SceneTransition : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit called! (This won't close the editor)");
+    }
+
+
+
+
+    // ========================== Scene Fade =====================================
+    public void FadeToScene(string sceneName)
+    {
+        StartCoroutine(FadeIn(sceneName));
+    }
+
+    IEnumerator FadeIn(string sceneName)
+    {
+        float t = 0;
+
+        while (t < fadeTime)
+        {
+            t += Time.deltaTime;
+            fadeGroup.alpha = t / fadeTime;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
+
+        // Now fade out after the scene loads
+        yield return StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut()
+    {
+        float t = 0;
+
+        while (t < fadeTime)
+        {
+            t += Time.deltaTime;
+            fadeGroup.alpha = 1 - (t / fadeTime);
+            yield return null;
+        }
     }
 }
