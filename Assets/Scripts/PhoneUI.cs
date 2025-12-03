dbaa927a2554d05eaa6d9f55100a73b641bed7eb
jokullsmari <jokullsmarigestsson@gmail.com>
2025-12-03 10:41:41 +0000

    AssetRecoveryThing :/

---
+++ Assets/Scripts/PhoneUI.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PhoneUI : MonoBehaviour
+{
+    public TextMeshProUGUI infoText;
+    public Button getInfoButton;
+
+    private void Start()
+    {
+        infoText.text = "Tap to view delivery info";
+        getInfoButton.onClick.AddListener(OnGetInfoClicked);
+    }
+
+    private void OnGetInfoClicked()
+    {
+        GameManager.Instance.GenerateNextCustomer();
+        infoText.text = GameManager.Instance.GetPhoneDescription();
+
+        // Make the button disappear after first click
+        getInfoButton.gameObject.SetActive(false);
+    }
+}
