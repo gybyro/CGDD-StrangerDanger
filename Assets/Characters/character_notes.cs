41589457f82eb66adcf232f639b257e671a33d7d
goji <100840760+gybyro@users.noreply.github.com>
2025-12-17 15:26:45 +0000

    .

--- Assets/Characters/character_notes.cs
+++ Assets/Characters/character_notes.cs
@@ -58,67 +58,67 @@
 using UnityEngine;
 using System;
 
-[Serializable]
-public class DialogueFile  // json file - called "dial_..."
-{
-    public string scene;
-    public DialogueLine[] lines;
-}
+// [Serializable]
+// public class DialogueFile  // json file - called "dial_..."
+// {
+//     public string scene;
+//     public DialogueLine[] lines;
+// }
 
-[Serializable]
-public class DialogueLine 
-{
-    public string id;
-    public string speaker;
-    public string color;
-    public string text;
-    public string portrait;
-    public string sound;
-    public string trigger;
-    public string animationTriggerDoor;
-    public string showChar;  // character.Show() or character.Hide()
-    public float waitSeconds;
-    public float typeSpeed;
-    public string next;
-    public int tips;
-    public string nextDialFile;
-    public bool finished;
+// [Serializable]
+// public class DialogueLine 
+// {
+//     public string id;
+//     public string speaker;
+//     public string color;
+//     public string text;
+//     public string portrait;
+//     public string sound;
+//     public string trigger;
+//     public string animationTriggerDoor;
+//     public string showChar;  // character.Show() or character.Hide()
+//     public float waitSeconds;
+//     public float typeSpeed;
+//     public string next;
+//     public int tips;
+//     public string nextDialFile;
+//     public bool finished;
 
-    public bool timerStart;
-    public float countDownSeconds;   // e.g. 10
-    public string timeOutNext;       // default path when time ends (wait happens)
-    public string escapeNext;        // where to go if player chooses to leave
-    public string escapeText;        // optional UI label e.g "leave" (nice to have) 
-
-
-
-    public string type;      // "choice", "random", or null
-    public string prompt;
-    public ChoiceOption[] options;
-    public RandomChoice[] choices;
-}
-[Serializable]
-public class ChoiceOption {
-    public string text;
-    public string next;
-}
-[Serializable]
-public class RandomChoice {
-    public int weight;
-    public string next;
-}
+//     public bool timerStart;
+//     public float countDownSeconds;   // e.g. 10
+//     public string timeOutNext;       // default path when time ends (wait happens)
+//     public string escapeNext;        // where to go if player chooses to leave
+//     public string escapeText;        // optional UI label e.g "leave" (nice to have) 
 
 
-//// for door bg
-[Serializable]
-public class SpriteColorSet
-{
-    public string setName;
-    public Sprite doorS;
-    public Sprite outerWallS;
-    public Sprite innerWallS;
-    public Sprite floorS;
-}
+
+//     public string type;      // "choice", "random", or null
+//     public string prompt;
+//     public ChoiceOption[] options;
+//     public RandomChoice[] choices;
+// }
+// [Serializable]
+// public class ChoiceOption {
+//     public string text;
+//     public string next;
+// }
+// [Serializable]
+// public class RandomChoice {
+//     public int weight;
+//     public string next;
+// }
+
+
+// //// for door bg
+// [Serializable]
+// public class SpriteColorSet
+// {
+//     public string setName;
+//     public Sprite doorS;
+//     public Sprite outerWallS;
+//     public Sprite innerWallS;
+//     public Sprite floorS;
+// }
 
 
 
@@ -135,51 +135,51 @@ public class SpriteColorSet
 //     public DayInWeek sunday;
 // }
 
-[Serializable]
-public class InGameWeek
-{
-    public DayInWeek[] dayInWeek;
-}
+// [Serializable]
+// public class InGameWeek
+// {
+//     public DayInWeek[] dayInWeek;
+// }
 
-[Serializable]
-public class DayInWeek
-{
-    public TimeSlot[] timeSlots;
-}
+// [Serializable]
+// public class DayInWeek
+// {
+//     public TimeSlot[] timeSlots;
+// }
 
-[Serializable]
-public class TimeSlot
-{
-    public PlayerDialogueLine[] currentLinesToPlay;
-}
+// [Serializable]
+// public class TimeSlot
+// {
+//     public PlayerDialogueLine[] currentLinesToPlay;
+// }
 
-[Serializable]
-public class PlayerDialogueLine
-{
-    public Color color = Color.white;
-    [TextArea(2, 5)]
-    public string text;
-    public AudioClip sound;
-    public float waitSeconds;
-    public float typeSpeed;
-}
+// [Serializable]
+// public class PlayerDialogueLine
+// {
+//     public Color color = Color.white;
+//     [TextArea(2, 5)]
+//     public string text;
+//     public AudioClip sound;
+//     public float waitSeconds;
+//     public float typeSpeed;
+// }
 
 
 
-[Serializable]
-public struct CRTPreset
-{
-    public float pixelSize;
-    public float distortionStrength;
-    public float distortionSmoothing;
-    public float rgbStrength;
-    public float scanlineStrength;
-    public float scanlineSize;
-    public float randomWear;
-    public float aberrationStrength;
-    public float trackingJitter;
-    public float trackingStrength;
-    public float trackingColorDamage;
-    public float contrast;
-    public float brightness;
-}
+// [Serializable]
+// public struct CRTPreset
+// {
+//     public float pixelSize;
+//     public float distortionStrength;
+//     public float distortionSmoothing;
+//     public float rgbStrength;
+//     public float scanlineStrength;
+//     public float scanlineSize;
+//     public float randomWear;
+//     public float aberrationStrength;
+//     public float trackingJitter;
+//     public float trackingStrength;
+//     public float trackingColorDamage;
+//     public float contrast;
+//     public float brightness;
+// }
